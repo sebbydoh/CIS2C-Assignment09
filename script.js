f2213b1f6f89d4ede7f87363059798e119968b03
Encinia <sebastienencinia@my.whccd.edu>
2020-03-11 20:57:20 -0700

    Forgot to save before my last commit

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("I loved the fact that the textbook was free and easily findable through the internet.  I've googled the textbook many times during the lecture, and it's very easy to find.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("I can't think of anything to change about the course.");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I learned everything that I expected to learn. Good solid fundamentals.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("I didn't fully understand loops before, now I understand what they do and why they are very useful.");
